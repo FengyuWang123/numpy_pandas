1e658f35b2cc050c39b825b0be9139849cdaf25d
395413032@qq.com <395413032@qq.com>
2019-03-27 00:56:48 +0800

    change 1

--- numpy_pandas.py
+++ numpy_pandas.py
@@ -303,5 +303,39 @@ print(b is a)
 # import pandas as pd
 # import numpy as np
 
-s = pd.Series([1,3,6,np.nan,44,1])
+s = pd.Series([1,3,6,np.nan,44,1])  # 给一个列表  np.nan 表示什么都没有NONE
 print(s)
+
+# 注：在pandas中保存形式用float 32或64 保存形式
+
+dates =  pd.date_range('20160101', periods=6) # 描述行的索引
+print(dates)
+
+# 建立data framework
+df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=['a','b','c','d'])
+print(df)
+
+df1 = pd.DataFrame(np.arange(12).reshape((3,4)))
+print(df1)
+
+# 字典形式
+df2 = pd.DataFrame({'A' : 1.,
+                    'B' : pd.Timestamp('20130102'),
+                    'C' : pd.Series(1,index=list(range(4)),dtype='float32'),
+                    'D' : np.array([3] * 4,dtype='int32'),
+                    'E' : pd.Categorical(["test","train","test","train"]),
+                    'F' : 'foo'})        
+                    
+print(df2)
+print(df2.dtypes)
+print(df2.index)
+print(df2.columns)
+print(df2.values)
+print(df2.describe())
+print(df2.T) # transport 矩阵 逆矩阵
+print(df2.sort_index(axis=1, ascending=False)) # axis=1是对描述进行排序，0是对index
+print(df2.sort_values(by='E')) # 对值进行排序
+
+
+
+
